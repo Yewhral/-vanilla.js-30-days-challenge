0bf819522fcbdb75ec09b1b12fb87c3e1490b601
Yewhral <zabrzyjewski@gmail.com>
2017-07-18 18:05:31 +0200

    day6 exercise finished, hardest so far

--- day6-AJAX/js/app.js
+++ day6-AJAX/js/app.js
@@ -1,1 +1,35 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+const prom = fetch(endpoint); // woah, a promise!
+
+prom.then(blob => blob.json())
+    .then(data => cities.push(...data));
+
+function findMatches(words, cities) {
+    return cities.filter(place => {
+        const regexp = new RegExp(words, 'gi');
+        return place.city.match(regexp) || place.state.match(regexp);
+    })
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, cities);
+    suggestions.innerHTML = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `<li>
+                <span class="name">${cityName},${stateName}</span>
+                <span class="population">${place.population}</span>
+                </li>`})
+        .join('');
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('keyup', displayMatches);
+
+
