083071dca11077facabc421d64d2059c2d5de734
Yewhral <zabrzyjewski@gmail.com>
2017-07-22 11:42:53 +0200

    day12 exercise finished

---
+++ day12-key-sequence/js/app.js
@@ -0,0 +1,12 @@
+const pressed = [];
+const secretCode = 'cheater';
+
+window.addEventListener('keyup', (e) => {
+   console.log(e.key);
+   pressed.push(e.key);
+   pressed.splice(-secretCode.length-1, pressed.length - secretCode.length);
+   console.log(pressed);
+   if(pressed.join('').includes(secretCode)) {
+       cornify_add();
+   }
+});
