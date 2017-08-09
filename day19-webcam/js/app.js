b343704b2f40acfc52d07971bef165fed84988db
Yewhral <zabrzyjewski@gmail.com>
2017-08-09 16:27:44 +0200

    day19 entry point

---
+++ day19-webcam/js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Doethe on 2017-08-09.
+ */
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
