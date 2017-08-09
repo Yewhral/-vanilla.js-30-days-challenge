481d80464e0aa02e1c2ef56538ada40abdb90800
Yewhral <zabrzyjewski@gmail.com>
2017-08-09 16:58:54 +0200

    day19 camera grabbed and displayed on canvas

--- day19-webcam/js/app.js
+++ day19-webcam/js/app.js
@@ -1,8 +1,30 @@
-/**
- * Created by Doethe on 2017-08-09.
- */
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const snap = document.querySelector('.snap')
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({ video:true, audio:false}).
+        then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+    })
+        .catch(err => {
+            console.error(`Well, this is a camera project, you must enable that stuff`, err);
+        })
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+getVideo();
